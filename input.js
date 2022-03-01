23df7a6d655754644febb77e49c6f7f69d14a119
Regan <52940978+reganjs@users.noreply.github.com>
2022-03-01 14:53:28 -0700

    Added movement keys and chat interaction capability

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) { 
+  connection = conn;
+
 const stdin = process.stdin;
   stdin.on("data", handleUserInput);
   stdin.setRawMode(true);
@@ -8,13 +12,29 @@ const stdin = process.stdin;
   return stdin;
 };
 
-
 // handles user input - exits system with ctrl + c
 const handleUserInput = function (key) {
   if (key === "\u0003") {
-    console.log("User exited the terminal manually!");
+// logs Thanks for playing, come back soon! when the player exits the game
+    console.log("Thanks for playing, come back soon!");
     process.exit();
   }
+
+//moves the snake character left, right, up or down dependant on the key you press using a, w, s or d
+  if (key === "w") connection.write("Move: up");
+  if (key === "a") connection.write("Move: left");
+  if (key === "s") connection.write("Move: down");
+  if (key === "d") connection.write("Move: right");
+//moves the snake character left, right, up or down dependant on the key you press using the left, right, up and down arrow keys
+  if (key === "38") connection.write("Move: up");
+  if (key === "37") connection.write("Move: left");
+  if (key === "40") connection.write("Move: down");
+  if (key === "39") connection.write("Move: right");
+//logs various text chat dependant on which character you input
+  if (key === "g") connection.write("Say: Hello! I am a snake");
+  if (key === "t") connection.write("Say: I'm enjoying playing snake instead of coding");
+  if (key === "q") connection.write("Say: My ultimate is charging! Wait... what ultimate? THIS IS SNAKE!");
+  if (key === "f") connection.write("Say: Use POWERUP: GROWTH!");
 };
 
 module.exports = setupInput;
