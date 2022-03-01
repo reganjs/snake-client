a0c3c463ad191c04ff18684f6ec35bae3567ce28
Regan <52940978+reganjs@users.noreply.github.com>
2022-02-28 22:06:50 -0700

    Added initial code

--- play.js
+++ play.js
@@ -1,1 +1,32 @@
+const net = require("net");
 
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+   // host: "",
+   // port: ,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", msg => {
+    console.log("Connected!");
+    console.log(`Connect message: ${msg}`);
+  });
+
+  conn.on("data", msg => {
+    console.log(`Server message: ${msg}`);
+  });
+
+  // print msg sent by server
+  conn.on("end", msg => {
+    console.log("Ended!");
+    console.log(`End message: ${msg}`);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
