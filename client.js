cee3328d3d6687c9a0d7c1d4eedad93f7dc3fa96
Regan <52940978+reganjs@users.noreply.github.com>
2022-03-01 14:25:08 -0700

    Added events to client.js

--- client.js
+++ client.js
@@ -1,4 +1,35 @@
-const connect = require("./client");
+const { IP, PORT, CLIENT_NAME } = require("./constants");
+const net = require("net");
 
-console.log("Connecting ...");
-connect();
+// establishes a connection with the game server and returns an object
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT,
+  });
+
+  // interprets incoming data as text
+  conn.setEncoding("utf8");
+
+  // event handler
+  // logs Connected! when the connection is successfully established
+  conn.on("connect", () => {
+    console.log("Connected!");
+    conn.write(`Name: ${CLIENT_NAME}`);
+  });
+
+  // handles events during idle state
+  conn.on("data", msg => {
+    console.log(`Server message: ${msg}`);
+  });
+
+  // logs Ended! when the connection ends
+  conn.on("end", () => {
+    console.log("Ended!");
+  });
+
+ // this 'conn' object gets returned and represents the client-server side connection
+  return conn;
+};
+
+module.exports = connect;
