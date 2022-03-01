a4d97e9c95e3ac719185ac186229a3d6f366fdb5
Regan <52940978+reganjs@users.noreply.github.com>
2022-03-01 15:01:42 -0700

    Updated constants.js

--- constants.js
+++ constants.js
@@ -2,8 +2,6 @@ const IP = "localhost";
 const PORT = 50541;
 const CLIENT_NAME = "RJS";
 
-//will add movement keys to this as well shortly, just have to change my input file first
-
 module.exports = {
   IP,
   PORT,
