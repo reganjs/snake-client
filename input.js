1d5812639d8d319e16c3da4d0ab6514137a529fd
Regan <52940978+reganjs@users.noreply.github.com>
2022-03-01 12:36:07 -0700

    Edited comments

--- input.js
+++ input.js
@@ -1,3 +1,4 @@
+// setup interface to handle user input from stdin
 const setupInput = function () {
 const stdin = process.stdin;
   stdin.on("data", handleUserInput);
@@ -8,10 +9,10 @@ const stdin = process.stdin;
 };
 
 
-//handles user input - exits system with ctrl + c
+// handles user input - exits system with ctrl + c
 const handleUserInput = function (key) {
   if (key === "\u0003") {
-    console.log("Client manually terminated!");
+    console.log("User exited the terminal manually!");
     process.exit();
   }
 };
