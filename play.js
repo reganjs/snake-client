7adf0c3e4fd559e01fbb59304a1218d9cdfe2813
Regan <52940978+reganjs@users.noreply.github.com>
2022-03-01 10:34:19 -0700

    Fixed error?

--- play.js
+++ play.js
@@ -1,5 +1,5 @@
-const { connect } = require("./client");
-const { setupInput } = require("./input");
+const connect = require("./client");
+const setupInput = require("./input");
 
 // logs connecting
 console.log("Connecting ...");
