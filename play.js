ae5ee66161ecf4060cd5c5df630d4b92d2b5774e
Regan <52940978+reganjs@users.noreply.github.com>
2022-03-01 10:33:29 -0700

    Changed setupInput fn to it's own file

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
-const connect = require("./client");
-
-// stdin 
-// setupInput listens for keyboard input and reacts to it
-const setupInput = function () {
-const stdin = process.stdin;
-  stdin.on("data", handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-// handles data input from the user
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    console.log("Client manually terminated!");
-    process.exit();
-  }
-};
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 // logs connecting
 console.log("Connecting ...");
